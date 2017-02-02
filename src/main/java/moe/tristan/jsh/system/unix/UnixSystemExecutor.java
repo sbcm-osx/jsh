64f60703b9d41f815c8688d1a074f8c282def078
Tristan Deloche <tristandeloche@gmail.com>
2017-02-02 13:45:07 +0100

    Add an implementation of the SystemExecutor for unix systems.

---
+++ src/main/java/moe/tristan/jsh/system/unix/UnixSystemExecutor.java
@@ -0,0 +1,52 @@
+package moe.tristan.jsh.system.unix;
+
+import lombok.extern.slf4j.Slf4j;
+import moe.tristan.jsh.lang.ListUtils;
+import moe.tristan.jsh.model.Command;
+import moe.tristan.jsh.model.Session;
+import moe.tristan.jsh.system.AbstractSystemExecutor;
+import moe.tristan.jsh.system.SystemExecutor;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.Optional;
+
+/**
+ * This is a proposition of implementation of the {@link SystemExecutor}
+ * for unix platforms. Because this is the default there are not many
+ * workarounds associated.
+ */
+@Slf4j
+public class UnixSystemExecutor extends AbstractSystemExecutor {
+
+    @Override
+    public Optional<OutputStream> execute(final Command command) {
+        final ProcessBuilder processBuilder = new ProcessBuilder();
+
+        processBuilder.command(
+                ListUtils.listOf(
+                        Collections.singletonList(command.getFunction()),
+                        command.getArguments()
+                )
+        );
+
+        processBuilder.directory(
+                Session.getSession()
+                        .getCurrentDirectory()
+                        .toAbsolutePath()
+                        .toFile()
+        );
+
+        processBuilder.inheritIO();
+
+        try {
+            final Process process = processBuilder.start();
+            return Optional.ofNullable(process.getOutputStream());
+        } catch (final IOException e) {
+            log.error("The command you entered was invalid : "+e.getMessage());
+        }
+
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/moe/tristan/jsh/system/unix/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all unix-only
+ * implementation details.
+ */
+package moe.tristan.jsh.system.unix;
